ab9cbacc1307bcbb2cf21111c94d8b3d854d6e6e
Piat Jonathan <piat.jonathan@gmail.com>
2017-03-06 11:02:37 +0100

    Removed build files

--- resampling_eigen/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_videoio -lopencv_imgproc -lopencv_highgui -lopencv_core -lopencv_objdetect -lopencv_imgcodecs
-
